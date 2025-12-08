a987f5c6d6e3c79e9f3c88734eab968483adc31d
Tito Candra <34393188+tcandra24@users.noreply.github.com>
2025-12-08 08:19:51 +0700

    Rev add clerk auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { ClerkProvider, SignInButton, SignUpButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 
 const geistSans = Geist({
